c3d7c4561e2eef5fdd16bfa7554f74670299daa8
Nikhil-codecracker <nikhilgupta1489@gmail.com>
2020-10-21 21:08:52 +0530

    api request handled

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-var uv = ["No protection needed. You can safely stay outside", "Protection needed. Seek shade during late morning through mid-afternoon.", "Extra protection needed. Be careful outside"]
+var uvmap = ["No protection needed. You can safely stay outside", "Protection needed. Seek shade during late morning through mid-afternoon.", "Extra protection needed. Be careful outside, especially during afternoon"];
 
-var uvScale = ["Extreme","Very High","High","Moderate","Low"];
 var visibleIndex = ["Excellent","Very Good","Good","Moderate","Poor"];
 var humidityIndex = [" Very Dry","Dry","Moderate","Humid","Very Humid"];
 var airQualityIndex = ["Good","Moderate","Unhealthy","Very Unhealthy","Hazardous"];
@@ -89,8 +88,6 @@ function getValues(city="New Delhi"){
 
     main.onload = (e) => {
         var res = JSON.parse(main.responseText);
-        var city = res.location.name;
-        var country = res.location.country;
         var temp = res.current.temp_c;
         var cond = res.current.condition.text;
         var wind_speed = res.current.wind_kph;
@@ -101,11 +98,12 @@ function getValues(city="New Delhi"){
         var lat = res.location.lat;
         var lng = res.location.lon;
         var daydata = res.forecast;
-        let daytemp = new Array(5);
+        let daytemp = new Array();
         for (var i=0;i<3;i++){
             // console.log(daydata.forecastday[i].day.avgtemp_c)
             daytemp.push(daydata.forecastday[i].day.avgtemp_c)
         }
+        console.log(daytemp)
         var sunrise = daydata.forecastday[0].astro.sunrise;
         var sunset = daydata.forecastday[0].astro.sunset;
         var rain = daydata.forecastday[0].day.daily_will_it_rain;
@@ -117,12 +115,86 @@ function getValues(city="New Delhi"){
         var day =  dayOfWeek[daynum];
         var month = monthOfYear[monthnum];
         var weathercond = daydata.forecastday[0].day.condition.text;
+        var lastDay = Math.round((temp+daytemp[1]+daytemp[2])/3);
 
         var todayDate = day + ", " + date + " " + month + " " + year;
         //console.log(todayDate);
 
         // console.log(sunrise,sunset)
-         console.log(city, country, temp, cond, wind_speed, wind_dir, rain, humidity, visibility, uv);
+         //console.log(temp, cond, wind_speed, wind_dir, rain, humidity, visibility, uv);
+
+
+         document.getElementById("date").innerHTML = todayDate;
+         document.getElementById("today").innerHTML = temp;
+         document.getElementById("weather").innerHTML = weathercond;
+         document.getElementById("windspeedright").innerHTML = wind_speed;
+         document.getElementById("humidityright").innerHTML = humidity + "%";
+         document.getElementById("precipitation").innerHTML = rain + "%";
+         document.getElementById("day").innerHTML = Math.round(temp) +'<span class="unitx"><span class="cel"><sup>&#8451;</sup></span></span>';
+         document.getElementById("day1").innerHTML = Math.round(daytemp[1]) + '<span class="unitx"><span class="cel"><sup>&#8451;</sup></span></span>';
+         document.getElementById("day2").innerHTML = Math.round(daytemp[2]) + '<span class="unitx"><span class="cel"><sup>&#8451;</sup></span></span>';
+         document.getElementById("day3").innerHTML = lastDay + '<span class="unitx"><span class="cel"><sup>&#8451;</sup></span></span>';
+         document.getElementById("daytwo").innerHTML = dayOfWeek[daynum+2];
+         document.getElementById("daythree").innerHTML = dayOfWeek[daynum+3];
+         document.getElementById("uv-index").innerHTML = uv;
+         if (uv <= 3) {
+             document.getElementById("uv-desc").innerHTML = uvmap[0]
+         }
+         else if (uv <= 7) {
+             document.getElementById("uv-desc").innerHTML = uvmap[1]
+         }
+         else {
+             document.getElementById("uv-desc").innerHTML = uvmap[2]
+         }
+ 
+ 
+ 
+         document.getElementById("sunrise").innerHTML = sunrise;
+         document.getElementById("sunset").innerHTML = sunset;
+         document.getElementById("wind-speed").innerHTML = wind_speed;
+         document.getElementById("wind-dir").innerHTML = wind_dir;
+ 
+         //Visibility
+         document.getElementById("vis-val").innerHTML = visibility;
+         if (visibility <= 2) {
+             document.getElementById("vis-status").innerHTML = "Poor";
+         }
+         else if (visibility <= 9) {
+             document.getElementById("vis-status").innerHTML = "Moderate";
+         }
+         else if (visibility <= 19) {
+             document.getElementById("vis-status").innerHTML = "Good";
+         }
+         else if (visibility <= 39) {
+             document.getElementById("vis-status").innerHTML = "Very Good";
+         }
+         else {
+             document.getElementById("vis-status").innerHTML = "Excellent";
+         }
+ 
+         //Humidity
+         console.log(humidity)
+         document.getElementById("humidity").innerHTML = humidity;
+         if (humidity <= 20) {
+             document.getElementById('humidity-status').innerHTML = "Very Dry";
+         }
+ 
+         else if (humidity <= 30) {
+             document.getElementById('humidity-status').innerHTML = "Dry";
+         }
+ 
+         else if (humidity <= 45) {
+             document.getElementById('humidity-status').innerHTML = "Moderate";
+         }
+ 
+         else if (humidity <= 60) {
+             document.getElementById('humidity-status').innerHTML = "Humid";
+         }
+ 
+         else {
+             document.getElementById('humidity-status').innerHTML = "Very Humid"
+         }
+
     }
 
     //AirQuality Index api Call
@@ -136,7 +208,32 @@ function getValues(city="New Delhi"){
         var aqi_val = resaqi.data.current.pollution.aqius
         // console.log(resaqi)
         console.log(aqi_val);
+
+        document.getElementById('aqi-val').innerHTML = aqi_val;
+        if (aqi_val < 50) {
+            document.getElementById('aqi-status').innerHTML = "Good";
+        }
+
+        else if (aqi_val < 150) {
+            document.getElementById('aqi-status').innerHTML = "Moderate";
+        }
+
+        else if (aqi_val < 200) {
+            document.getElementById('aqi-status').innerHTML = "Unhealthy";
+        }
+
+        else if (aqi_val < 500) {
+            document.getElementById('aqi-status').innerHTML = "Very Unhealthy";
+        }
+
+        else {
+            document.getElementById('aqi-status').innerHTML = "Hazardous";
+        }
     }
+
+    document.getElementById("city").innerHTML = city;
+    document.getElementById("country").innerHTML = dict[city].country;
+    
 }
 
 
