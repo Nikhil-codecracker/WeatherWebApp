5076187b53d1e8f60eb6e627c4da9aadf08e3cb6
Nikhil-codecracker <nikhilgupta1489@gmail.com>
2020-10-22 13:01:46 +0530

    search bar added

--- script.js
+++ script.js
@@ -243,3 +243,335 @@ function getValues(city = "New Delhi") {
 
 getValues();
 
+document.getElementById("location").onclick = () =>{
+    document.getElementById("innerconsole").style.display = "none";
+    
+}
+
+document.getElementById("dashboard").onclick = () =>{
+    document.getElementById("innerconsole").style.display = "block";
+}
+
+
+
+const users = [
+    {
+        "id": 0,
+        "name": "Kartikeya Agarwal",
+        "username": "Kartikeya72001",
+        "email": "kartikeya72001@gmail.com",
+        "address": {
+          "street": "Secotr 13",
+          "suite": "A-3/701",
+          "city": "New Delhi",
+          "zipcode": "110085",
+          "geo": {
+            "lat": "28.6139",
+            "lng": "77.2090"
+          }
+        },
+        "phone": "9718502020",
+        "website": "kartikeya72001.github.io",
+        "company": {
+          "name": " ",
+          "catchPhrase": "",
+          "bs": "Web Development"
+        }
+      },
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+          "lat": "-37.3159",
+          "lng": "81.1496"
+        }
+      },
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+      }
+    },
+    {
+      "id": 2,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "email": "Shanna@melissa.tv",
+      "address": {
+        "street": "Victor Plains",
+        "suite": "Suite 879",
+        "city": "Wisokyburgh",
+        "zipcode": "90566-7771",
+        "geo": {
+          "lat": "-43.9509",
+          "lng": "-34.4618"
+        }
+      },
+      "phone": "010-692-6593 x09125",
+      "website": "anastasia.net",
+      "company": {
+        "name": "Deckow-Crist",
+        "catchPhrase": "Proactive didactic contingency",
+        "bs": "synergize scalable supply-chains"
+      }
+    },
+    {
+      "id": 3,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "email": "Nathan@yesenia.net",
+      "address": {
+        "street": "Douglas Extension",
+        "suite": "Suite 847",
+        "city": "McKenziehaven",
+        "zipcode": "59590-4157",
+        "geo": {
+          "lat": "-68.6102",
+          "lng": "-47.0653"
+        }
+      },
+      "phone": "1-463-123-4447",
+      "website": "ramiro.info",
+      "company": {
+        "name": "Romaguera-Jacobson",
+        "catchPhrase": "Face to face bifurcated interface",
+        "bs": "e-enable strategic applications"
+      }
+    },
+    {
+      "id": 4,
+      "name": "Patricia Lebsack",
+      "username": "Karianne",
+      "email": "Julianne.OConner@kory.org",
+      "address": {
+        "street": "Hoeger Mall",
+        "suite": "Apt. 692",
+        "city": "South Elvis",
+        "zipcode": "53919-4257",
+        "geo": {
+          "lat": "29.4572",
+          "lng": "-164.2990"
+        }
+      },
+      "phone": "493-170-9623 x156",
+      "website": "kale.biz",
+      "company": {
+        "name": "Robel-Corkery",
+        "catchPhrase": "Multi-tiered zero tolerance productivity",
+        "bs": "transition cutting-edge web services"
+      }
+    },
+    {
+      "id": 5,
+      "name": "Chelsey Dietrich",
+      "username": "Kamren",
+      "email": "Lucio_Hettinger@annie.ca",
+      "address": {
+        "street": "Skiles Walks",
+        "suite": "Suite 351",
+        "city": "Roscoeview",
+        "zipcode": "33263",
+        "geo": {
+          "lat": "-31.8129",
+          "lng": "62.5342"
+        }
+      },
+      "phone": "(254)954-1289",
+      "website": "demarco.info",
+      "company": {
+        "name": "Keebler LLC",
+        "catchPhrase": "User-centric fault-tolerant solution",
+        "bs": "revolutionize end-to-end systems"
+      }
+    },
+    {
+      "id": 6,
+      "name": "Mrs. Dennis Schulist",
+      "username": "Leopoldo_Corkery",
+      "email": "Karley_Dach@jasper.info",
+      "address": {
+        "street": "Norberto Crossing",
+        "suite": "Apt. 950",
+        "city": "South Christy",
+        "zipcode": "23505-1337",
+        "geo": {
+          "lat": "-71.4197",
+          "lng": "71.7478"
+        }
+      },
+      "phone": "1-477-935-8478 x6430",
+      "website": "ola.org",
+      "company": {
+        "name": "Considine-Lockman",
+        "catchPhrase": "Synchronised bottom-line interface",
+        "bs": "e-enable innovative applications"
+      }
+    },
+    {
+      "id": 7,
+      "name": "Kurtis Weissnat",
+      "username": "Elwyn.Skiles",
+      "email": "Telly.Hoeger@billy.biz",
+      "address": {
+        "street": "Rex Trail",
+        "suite": "Suite 280",
+        "city": "Howemouth",
+        "zipcode": "58804-1099",
+        "geo": {
+          "lat": "24.8918",
+          "lng": "21.8984"
+        }
+      },
+      "phone": "210.067.6132",
+      "website": "elvis.io",
+      "company": {
+        "name": "Johns Group",
+        "catchPhrase": "Configurable multimedia task-force",
+        "bs": "generate enterprise e-tailers"
+      }
+    },
+    {
+      "id": 8,
+      "name": "Nicholas Runolfsdottir V",
+      "username": "Maxime_Nienow",
+      "email": "Sherwood@rosamond.me",
+      "address": {
+        "street": "Ellsworth Summit",
+        "suite": "Suite 729",
+        "city": "Aliyaview",
+        "zipcode": "45169",
+        "geo": {
+          "lat": "-14.3990",
+          "lng": "-120.7677"
+        }
+      },
+      "phone": "586.493.6943 x140",
+      "website": "jacynthe.com",
+      "company": {
+        "name": "Abernathy Group",
+        "catchPhrase": "Implemented secondary concept",
+        "bs": "e-enable extensible e-tailers"
+      }
+    },
+    {
+      "id": 9,
+      "name": "Glenna Reichert",
+      "username": "Delphine",
+      "email": "Chaim_McDermott@dana.io",
+      "address": {
+        "street": "Dayna Park",
+        "suite": "Suite 449",
+        "city": "Bartholomebury",
+        "zipcode": "76495-3109",
+        "geo": {
+          "lat": "24.6463",
+          "lng": "-168.8889"
+        }
+      },
+      "phone": "(775)976-6794 x41206",
+      "website": "conrad.com",
+      "company": {
+        "name": "Yost and Sons",
+        "catchPhrase": "Switchable contextually-based project",
+        "bs": "aggregate real-time technologies"
+      }
+    },
+    {
+      "id": 10,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "email": "Rey.Padberg@karina.biz",
+      "address": {
+        "street": "Kattie Turnpike",
+        "suite": "Suite 198",
+        "city": "Lebsackbury",
+        "zipcode": "31428-2261",
+        "geo": {
+          "lat": "-38.2386",
+          "lng": "57.2232"
+        }
+      },
+      "phone": "024-648-3804",
+      "website": "ambrose.net",
+      "company": {
+        "name": "Hoeger LLC",
+        "catchPhrase": "Centralized empowering task-force",
+        "bs": "target end-to-end models"
+      }
+    }
+  ]
+
+const conSearch = document.querySelector('.con-search')
+function handleRemove() {
+    conSearch.querySelector('input').value = ''
+    conSearch.classList.add('notValue')
+}
+function handleFocus(evt) {
+    if (evt.target.value) {
+        conSearch.classList.add('focus')
+    }
+}
+function handleBlur() {
+    conSearch.classList.remove('focus')
+}
+
+function handleSearch(evt) {
+    const value = evt.target.value
+    const newUsers = users.filter((user) => {
+        delete user.address
+        delete user.company
+        delete user.phone
+        delete user.id
+        delete user.username
+        const string = JSON.stringify(user).toLowerCase()
+        if (string.indexOf(value.toLowerCase()) !== -1) {
+            return user
+        }
+    })
+    renderResults(newUsers, value)
+}
+function Capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function renderResults(results, value) {
+    const conResults = document.querySelector('.con-results')
+    conResults.innerHTML = ''
+    
+    if (!value) {
+        conSearch.classList.remove('focus')
+        conSearch.classList.add('notValue')
+        return    
+    } 
+    conSearch.classList.remove('notValue')
+    conSearch.classList.add('focus')
+    results.forEach((result) => {
+        const resultElement = document.createElement('div')
+        resultElement.className = 'result'
+        const title = document.createElement('h5')
+        const text = document.createElement('p')
+        const web = document.createElement('div')
+        web.className = 'web'
+        const capitalize = Capitalize(result.name)
+        title.innerHTML = capitalize.replace(value, `<b>${value}</b>`)
+        text.innerHTML = result.email.toLowerCase().replace(value, `<b>${value}</b>`)
+        web.innerHTML = result.website.toLowerCase().replace(value, `<b>${value}</b>`)
+        resultElement.appendChild(title)
+        resultElement.appendChild(text)
+        resultElement.appendChild(web)
+        resultElement.classList.add('hidden')
+        conResults.appendChild(resultElement)
+        setTimeout(() => {
+            resultElement.classList.remove('hidden')
+        }, 20);
+    })
+}
